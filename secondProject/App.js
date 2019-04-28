ccd099ef8dff5b09753cd2316a0b9cf0616a5847
Satish Pandey <satish.pandey@noqcart.com>
2019-04-28 11:18:31 +0530

    re-arranging the components.

--- secondProject/App.js
+++ secondProject/App.js
@@ -1,84 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput,Button,ScrollView} from 'react-native';
-import ListItem from './src/component/ListItem/ListItem';
+import { StyleSheet, View,TextInput,Button,ScrollView} from 'react-native';
+
+import PlaceInput from "./src/component/PlaceInput/PlaceInput";
+import PlaceList from "./src/component/PlaceList/PlaceList";
 
 export default class App extends React.Component {
 
   state={
-    placeName:'',
     places:[]
   }
 
-  placeNameChangeHandler= val =>{
-    this.setState({
-      placeName:val
-    })
-  }
 
-  placeSubmitHandler =()=>{
-    if(this.state.placeName.trim()===""){
-      return;
-    }
-    this.setState(
-      prevState => {
-        return {
-          places:prevState.places.concat(prevState.placeName)
-        }
-      }
-    );
-    this.setState({
-      placeName:''
-    })
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat({
+          key: Math.random(),
+          value: placeName
+        })
+      };
+    });
+  };
 
-  }
+  placeDeletedHandler = key => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter(place => {
+          return place.key !== key;
+        })
+      };
+    });
+  };
+
+  
+
   render() {
-    const placesOutput=this.state.places.map(
-      (place,i)=>(
-        <ListItem key={i} newPlace={place} 
-        onItemPressed={()=> alert('Item pressed -ID: '+i)} />
-      )
-    );
+
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.inputStyle}>
-          <TextInput   
-            style={styles.placeInput}
-            placeholder="awesome place"
-            value={this.state.placeName} 
-            onChangeText={this.placeNameChangeHandler}
-          />     
-          <Button title="Add" style={styles.placeButton} onPress={this.placeSubmitHandler}/>
-        </View>
-        <View>
-          {placesOutput}
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
+          places={this.state.places}
+          onItemDeleted={this.placeDeletedHandler}
+        />
+      </View>
       
     );
   }
 }
 
 //https://github.com/vhpoet/react-native-styling-cheat-sheet
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding:20,
-    backgroundColor: '#fff',
-   // alignItems: 'center',
-   // justifyContent: 'flex-start',
-  },
-  inputStyle:{
-   // flex: 1,
-   width:"100%",
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center"
-  },
-  placeInput:{
-    width:"70%",
-    borderColor:"black",borderWidth:1
-  },
-  placeButton:{
-    width:"30%"
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
